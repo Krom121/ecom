0794911cfc4e1d6d91d48ccfb4230aef518cd237
krom121 <stephenacgregor9@gmail.com>
2019-05-04 09:19:38 +0100

    blog views+models+urls

--- ecom/blog/models.py
+++ ecom/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -23,6 +24,10 @@ class Post(models.Model):
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, 
+                                                    self.publish.month, self.publish.day, self.slug])
     class Meta:
         ordering = ('-publish',)
 
